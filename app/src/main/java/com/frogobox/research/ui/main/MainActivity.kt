a6ce1d4b3f2f7ed507973d01288336f88e644e8b
amirisback <faisalamircs@gmail.com>
2023-02-02 10:06:45 +0700

    update: add copyable textview value

--- app/src/main/java/com/frogobox/research/ui/main/MainActivity.kt
+++ app/src/main/java/com/frogobox/research/ui/main/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.frogobox.research.ui.main
 
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.activity.viewModels
 import com.frogobox.research.core.BaseBindActivity
 import com.frogobox.research.databinding.ActivityMainBinding
+
 
 class MainActivity : BaseBindActivity<ActivityMainBinding>() {
 
@@ -31,7 +36,11 @@ class MainActivity : BaseBindActivity<ActivityMainBinding>() {
     override fun initView() {
         super.initView()
         binding.apply {
-
+            textView.setOnClickListener {
+                val cm: ClipboardManager = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
+                cm.setPrimaryClip(ClipData("text", arrayOf("text/plain"), ClipData.Item(textView.text)))
+                Toast.makeText(this@MainActivity, "Copied to clipboard", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
